c3e25c897e2d11e787a306671204cc9da6af6715
anclshar <an.cl.shar@gmail.com>
2019-03-21 08:39:37 +0300

    search components

---
+++ SearchComponent.js
@@ -0,0 +1,20 @@
+Vue.component('search-el', {
+    data(){
+        return {
+            userSearch: ''
+        }
+    },
+    methods: {
+        filter(){
+            let regexp  = new RegExp(this.userSearch, 'i')
+            this.filtered = this.products.filter(el => regexp.test(el.product_name))
+        }
+    },
+    template: `
+    <form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
+        <input type="text" class="search-field" v-model="userSearch">
+        <button class="btn-search" type="submit">
+            <i class="fas fa-search"></i>
+        </button>
+    </form>`
+})
